21ca4a4217c4685de39a5d41fc4a325991baf112
Jhony1974 <jhonys1@hotmail.com>
2023-06-11 12:09:16 -0500

    Add data context

---
+++ Sale.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sale.Shared.Entities;
+
+namespace Sale.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<County> Counties { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<County>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ Sale.Shared/Entities/County.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sale.Shared.Entities
+{
+    public class County
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "El campo es {0} obligatorio")]
+        [MaxLength(100, ErrorMessage = "el Campo {0} no puede tener mas de {1} caracteres")]
+        public string Name { get; set; } = null!;
+
+
+    }
+}
